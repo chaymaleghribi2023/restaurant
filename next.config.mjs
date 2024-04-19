3c9b005bd57a0d2ec33dead7941302b12cddd509
chaymaleghribi2023 <chirazleghribi@gmail.com>
2024-04-19 11:26:05 +0200

    Commit message

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['www.commande-pizzatime.fr','recette-catalogue.aureskonnect.com'],
+    }
+};
+
+export default nextConfig;
